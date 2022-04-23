b3766e056c71cc07c005dd6683ce388af64f4024
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-23 23:19:38 +0300

    schemas/userSchema.js

---
+++ studio/schemas/userSchema.js
@@ -0,0 +1,10 @@
+export const userSchema = {
+	name: 'users',
+	type: 'document',
+	title: 'Users',
+	fields: [
+		{ name: 'name', type: 'string', title: 'Name' },
+		{ name: 'name', type: 'string', title: 'Wallet Address' },
+		{ name: 'profileImage', type: 'string', title: 'Profile Image' },
+	],
+};
