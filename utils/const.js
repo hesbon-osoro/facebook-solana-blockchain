1f5dc75987e9e6e8af6a844a2d635a6e94137d27
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-29 23:39:01 +0300

    const.js

---
+++ utils/const.js
@@ -0,0 +1,21 @@
+import { clusterApiUrl, PublicKey } from '@solana/web3.js';
+import facebook_clone from './facebook_clone.json';
+
+export const CLUSTER =
+	process.env.REACT_APP_CLUSTER === 'mainnet'
+		? 'mainnet'
+		: process.env.REACT_APP_CLUSTER === 'testnet'
+		? 'testnet'
+		: 'devnet';
+
+export const SOLANA_HOST = process.env.REACT_APP_SOLANA_API_URL
+	? process.env.REACT_APP_SOLANA_API_URL
+	: CLUSTER === 'mainnet'
+	? clusterApiUrl('mainnet-beta')
+	: CLUSTER === 'testnet'
+	? clusterApiUrl('devnet')
+	: 'https://api.devnet.solana.com';
+
+export const STABLE_POOL_PROGRAM_ID = new PublicKey('');
+
+export const STABLE_POOL_IDL = facebook_clone;
